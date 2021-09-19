bf25ab0436e3800a1aa2a50635322f7c11114f6d
dranikpg <vladislav.oleshko@gmail.com>
2021-09-19 20:05:49 +0300

    Fix operation

--- lab-01-quizer/src/src/com.dranikpg.quizer/Operation.kt
+++ lab-01-quizer/src/src/com.dranikpg.quizer/Operation.kt
@@ -5,7 +5,7 @@ import javax.script.ScriptEngineManager
 enum class Operation(val c: Char, val f: (Int,Int) -> Int) {
     PLUS('+', { a, b -> a + b }),
     MINUS('-', fun (a, b) = a - b),
-    MULTIPLY('*', Int::plus),
+    MULTIPLY('*', Int::times),
     DIVIDE('/', fun(a,b) : Int { // I didn't come up with anything more dubious than dividing integers in javascript
         // lol lets also spin up a full scale javascript engine each time
         // takes some time
